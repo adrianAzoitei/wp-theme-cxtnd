13b39576d231d5775714613a9ab3e740a9b97031
Adrian Azoitei <azo.adrian@gmail.com>
2024-01-18 08:30:50 +0100

    chore: work on post card styling

--- template-parts/content-post-list.php
+++ template-parts/content-post-list.php
@@ -5,13 +5,13 @@
     ?>
     <a href="<?php the_permalink(); ?>">
         <div class="flex flex-col relative my-2">
-            <div class="flex flex-col py-5 px-4 hover:bg-post-hover rounded-lg">
-                <div class="flex flex-row items-center justify-between h-[10vh] w-[100%]">
-                    <div class="flex flex-col justify-start h-[100%]">
+            <div class="flex flex-col py-2 px-2 hover:bg-post-hover rounded-lg">
+                <div class="flex flex-row items-start justify-between h-[10vh] w-[100%]">
+                    <div class="flex flex-col justify-start h-[100%] mr-10">
                         <p class="text-lg font-bold"><?php the_title(); ?></p>
-                        <div class="font-light text-post-excerpt"><?php the_excerpt(); ?></div>
+                        <div class="font-light text-post-excerpt text-sm line-clamp-1"><?php the_excerpt(); ?></div>
                     </div>
-                    <img class="h-auto w-[10vh] mx-5" src="<?php echo the_post_thumbnail_url() ?>"/>
+                    <img class="post-thumbnail-img" src="<?php echo the_post_thumbnail_url() ?>"/>
                 </div>
                 <div class="h-[2vh] post-thumbnail-icons"></div>
             </div>
